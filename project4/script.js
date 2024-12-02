cf5d4f0b9d6d0497789ab0eb25e7398a972e3692
vrc73963 <vrc73963@hawkmail.hacc.edu>
2024-12-02 01:32:07 -0500

    commit

---
+++ project4/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+});
